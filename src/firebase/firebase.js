fa89d0f8e1c2a5651fb3db160e8da90ad82a6c3f
Dan Stronge <dnlstronge@gmail.com>
2022-12-14 14:44:21 +0000

    set up firebase config file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC7ivbo9fl0oCKU7zntfwhodrMHCw-8jc",
+    authDomain: "reading-list-22784.firebaseapp.com",
+    projectId: "reading-list-22784",
+    storageBucket: "reading-list-22784.appspot.com",
+    messagingSenderId: "853488986986",
+    appId: "1:853488986986:web:fe3b2a2623922e00d0d313"
+  };
