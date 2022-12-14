2acbf54c6628a42dc15460f705430746cfe36670
Dan Stronge <dnlstronge@gmail.com>
2022-12-14 14:57:41 +0000

    init firebaseConfig

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,5 @@
+import { initializeApp } from 'firebase/app'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBC7ivbo9fl0oCKU7zntfwhodrMHCw-8jc",
     authDomain: "reading-list-22784.firebaseapp.com",
@@ -6,3 +8,5 @@ const firebaseConfig = {
     messagingSenderId: "853488986986",
     appId: "1:853488986986:web:fe3b2a2623922e00d0d313"
   };
+
+initializeApp(firebaseConfig)
