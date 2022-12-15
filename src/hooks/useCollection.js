f555430d7b382966a02c719c8dbd80352af57602
Dan Stronge <dnlstronge@gmail.com>
2022-12-15 12:26:19 +0000

    create: complete useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,7 +1,25 @@
 import { useEffect, useState } from "react"
 import { db } from "../firebase/config"
 
+// firebase imports
 
-export const useCollection = () => {
+import { collection, onSnaphot } from "firebase/firestore"
 
+
+export const useCollection = (c) => {
+    const [ documents, setDocuments] = useState(null)
+
+    useEffect(() => {
+        let ref = collection(db, c)
+
+       const unsub = onSnaphot(ref, (snapshot) => {
+            let results = []
+            snapshot.docs.forEach(doc => {
+                results.push({...doc.data(), id: doc.id})
+            })
+            setDocuments(results)
+        })
+        return () => unsub()
+    }, [c])
+    return { documents }
 }
