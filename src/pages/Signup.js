11a5bc394c7419412e8fbce15cc5382482f99244
Dan Stronge <dnlstronge@gmail.com>
2022-12-17 10:12:11 +0000

    import usesignup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { error, signup } = useSignup('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
