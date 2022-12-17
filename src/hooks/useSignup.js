f2780e0fe27be529668aab0fb8a3170103f4a015
Dan Stronge <dnlstronge@gmail.com>
2022-12-17 10:02:19 +0000

    add state for error, call function for signup

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+// imports (FB)
+
+import { auth } from '..firebase/config'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+export const useSignup = () => {
+    const [error, setError ] = useState(null)
+
+    const signup = (email, password) => {
+        setError(null)
+        createUserWithEmailAndPassword(auth, email, password)
+    }
+    return ( error, signup )
+}
