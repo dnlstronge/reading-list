ff2c059d1fcd8515820d5aa6c4cc2d36f0d173c2
Dan Stronge <dnlstronge@gmail.com>
2022-12-19 10:08:51 +0000

    add dispatch (LOGIN)

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -8,12 +8,13 @@ import { auth } from "../firebase/config"
 
 export const useLogin = () => {
     const [error, setError ] = useState(null)
+    const { dispatch } = useAuthContext()
 
     const login = (email, password) => {
         setError(null)
         signInWithEmailAndPassword(auth, email, password)
         .then((res) => { 
-             console.log('User signed in: ', res.user)   
+            dispatch({ type: 'LOGIN', payload: res.user })   
         })
         .catch((err) => {
             setError(err.message)
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -14,7 +14,7 @@ export const useSignup = () => {
         setError(null)
         createUserWithEmailAndPassword(auth, email, password)
         .then((res) => { 
-             dispatch({ type: 'LOGIN', payload: res.user })
+             
         })
         .catch((err) => {
             setError(err.message)
